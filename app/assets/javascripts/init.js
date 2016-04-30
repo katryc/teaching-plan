0844139295648f7ab61ce1841897ffba0138b1c9
kat markava <katryc@outlook.com>
2016-04-30 16:44:50 -0400

    form

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -4,6 +4,12 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+    $('#textarea1').val('New Text');
+    $('#textarea1').trigger('autoresize');
+    $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
