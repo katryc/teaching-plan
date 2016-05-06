b1da8f148e9f113b650b35c8922166017a5da49f
kat markava <katryc@outlook.com>
2016-05-06 15:28:28 -0400

    pdf logo

--- app/pdfs/plans_pdf.rb
+++ app/pdfs/plans_pdf.rb
@@ -5,15 +5,49 @@ class PlansPdf < Prawn::Document
    @plan = plan
    @view = view
    logo
-   text "Your Teaching Plan #{@plan.id}"
+   title
+   time
+   body
+   regards_message
  end
 
 def logo
    logopath = "#{Rails.root}/app/assets/images/brand.png"
-   image logopath, :width => 197, :height => 91
+   image logopath, :width => 300, :height => 31
 
   end
 
+def title
+  move_down 30
+  text "TITLE: #{@plan.title}"
+end
 
+def time
+  move_down 10
+  text "Lesson time: #{@plan.start_time}  #{@plan.end_time}", :size => 20
+end
+
+def body
+  move_down 20
+  table plan_rows do
+      row(0).font_style = :bold
+      self.header = false
+      self.row_colors = ['DDDDDD', 'FFFFFF']
+      self.column_widths = [40, 300, 200]
+    end
+  end
+
+  def plan_rows
+    [["WARM UP","INTRO", "DRILL","PRACTICE","GAMES & ACTIVITIES","REVIEW"]] +
+    [@plan.warm_up, @plan.intro, @plan.drill, @plan.practice, @plan.gamesandactivities, @plan.review]
+
+end
+
+
+
+ def regards_message
+   move_down 30
+   text "Thank you for using TPplnr #{@plan.user_id}", :indent_paragraphs => 300
+ end
 
 end
