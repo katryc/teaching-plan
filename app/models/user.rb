ef9d4a8a3372dc8a4324fa83ea6f06ba4965e3f1
kat markava <katryc@outlook.com>
2016-05-07 22:59:38 -0400

    user destroy dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
-  has_many :plans
-  has_one :profile
+  has_many :plans, dependent: :destroy
+  has_one :profile, dependent: :destroy  
   before_create :build_profile #creates profile at user registration
-  validates :username, presence: true, length: { minimum: 4, maximum: 16 }  
+  validates :username, presence: true, length: { minimum: 4, maximum: 16 }
 end
