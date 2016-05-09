754afd9e68196e718ba1ef7e6746cf414595e776
kat markava <katryc@outlook.com>
2016-05-09 17:56:28 -0400

    rendered search

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :plans
+  get '/search' => 'plans#search', as: :search
   resources :profiles, only: [:edit]
   resources :users
   root 'home#index'
