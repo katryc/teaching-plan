c088e29038f258718b83c496b5a758c8f9473e26
kat markava <katryc@outlook.com>
2016-05-09 17:38:57 -0400

    added search method

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,5 +2,10 @@ class Plan < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   belongs_to :profile
-  
+
+
+  def self.search(term)
+     where("title LIKE ? OR content LIKE ?", "%#{term}%", "%#{term}%" )
+   end
+
 end
